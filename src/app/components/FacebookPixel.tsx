075f369a98f12985fd54da8c0cda8a37fd0f24a2
mupela nsowa <48857035+kizadek@users.noreply.github.com>
2024-08-15 08:30:19 +0200

    adding more hooks for state and data fetching
    adding more hooks for state and data fetching

--- src/app/components/FacebookPixel.tsx
+++ src/app/components/FacebookPixel.tsx
@@ -1,41 +1,36 @@
-// import React, { useEffect } from "react";
+import React from "react";
+import Script from "next/script";
 
-// declare global {
-//   interface Window {
-//     fbq: any;
-//   }
-// }
+interface Props {
+  containerId: string;
+}
+const FacebookPixel = ({ containerId }: Props) => {
+  return (
+    <>
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', ${containerId}); 
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1`}
+        />
+      </noscript>
+    </>
+  );
+};
 
-// const Facebook: React.FC = () => {
-//   useEffect(() => {
-//     !(function (f, b, e, v, n, t, s) {
-//       if (f.fbq) return;
-//       n = f.fbq = function () {
-//         n.callMethod
-//           ? n.callMethod.apply(n, arguments)
-//           : n.queue.push(arguments);
-//       };
-//       if (!f._fbq) f._fbq = n;
-//       n.push = n;
-//       n.loaded = !0;
-//       n.version = "2.0";
-//       n.queue = [];
-//       t = b.createElement(e);
-//       t.async = !0;
-//       t.src = v;
-//       s = b.getElementsByTagName(e)[0];
-//       s.parentNode.insertBefore(t, s);
-//     })(
-//       window,
-//       document,
-//       "script",
-//       "https://connect.facebook.net/en_US/fbevents.js"
-//     );
-//     fbq("init", "YOUR_PIXEL_ID"); // Insert your Pixel ID here
-//     fbq("track", "PageView");
-//   }, []);
-
-//   return null;
-// };
-
-// export default Facebook;
+export default FacebookPixel;
